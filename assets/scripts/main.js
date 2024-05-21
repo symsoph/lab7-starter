80b64ce4bb6fa155cf2cdd2bd7c72cc7dd19f846
Sofia Nguyen <120623425+symsoph@users.noreply.github.com>
2024-05-20 23:43:32 -0700

    done with expose

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -24,6 +24,7 @@ function getRecipesFromStorage() {
 	// A9. TODO - Complete the functionality as described in this function
 	//           header. It is possible in only a single line, but should
 	//           be no more than a few lines.
+	return JSON.parse(localStorage.getItem("recipes") || "[]");
 }
 
 /**
@@ -35,11 +36,33 @@ function getRecipesFromStorage() {
  */
 function addRecipesToDocument(recipes) {
 	// A10. TODO - Get a reference to the <main> element
+	let mainElement = document.querySelector('main');
 	// A11. TODO - Loop through each of the recipes in the passed in array,
 	//            create a <recipe-card> element for each one, and populate
 	//            each <recipe-card> with that recipe data using element.data = ...
-	//            Append each element to <main>
+	console.log(recipes);
+	console.log(recipes[0].titleTxt);
+	for (let i = 0; i < recipes.length; i++) {
+		let rCard = document.createElement('recipe-card');
+		// const oneRecipe = JSON.stringify(recipes[i]); // empty!
+		console.log(recipes[i].imgSrc);
+		rCard.data = { 
+			"imgSrc": recipes[i].imgSrc,
+			"imgAlt": recipes[i].imgAlt,
+			"titleLnk": recipes[i].titleLnk,
+			"titleTxt": recipes[i].titleTxt,
+			"organization": recipes[i].organization,
+			"rating": recipes[i].rating,
+			"numRatings": recipes[i].numRatings,
+			"lengthTime": recipes[i].lengthTime,
+			"ingredients": recipes[i].ingredients 
+		}; 
+		//            Append each element to <main>
+		mainElement.append(rCard);
+	}
 }
+
+// *** MAKE SURE THAT THE LOCAL STORAGE INPUT of array of recipes INTO CONSOLE IS RUNNING ***
 
 /**
  * Takes in an array of recipes, converts it to a string, and then
